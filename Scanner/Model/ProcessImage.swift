4e87eb7c09fff51575c629847e10e421ed54c961
Andy <andyzhang20013@gmail.com>
2020-05-28 17:38:36 -0500

    Basic setup of document scanner Create table view Need to create cells and connect cells with the document scanner

---
+++ Scanner/Model/ProcessImage.swift
@@ -0,0 +1,13 @@
+//
+//  ProcessImage.swift
+//  Scanner
+//
+//  Created by Andy Zhang on 5/28/20.
+//  Copyright © 2020 Andy Zhang. All rights reserved.
+//
+
+import UIKit
+import Vision
+import VisionKit
+
+
