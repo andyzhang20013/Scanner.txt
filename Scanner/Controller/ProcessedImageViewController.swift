6251288a14042c2928a4060a787092bbce96ab3c
Andy <andyzhang20013@gmail.com>
2020-05-30 12:51:21 -0500

    can scan images and get back text now Need to work on data persistence Need to fix transition between picture button and results screen

--- Scanner/Controller/ProcessedImageViewController.swift
+++ Scanner/Controller/ProcessedImageViewController.swift
@@ -7,14 +7,113 @@
 //
 
 import UIKit
-class ProcessedImageViewController: UIViewController{
+import Vision
+import VisionKit
+import RealmSwift
+class ProcessedImageViewController: UIViewController, VNDocumentCameraViewControllerDelegate{
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
+    var textRecognitionRequest = VNRecognizeTextRequest(completionHandler: nil)
+    private let textRecognitionWorkQueue = DispatchQueue(label: "MyVisionScannerQueue", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
  var selectedText : Data? //this will be set during the segue
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         textView.isEditable = false
-        textView.text = selectedText as? String ?? "Invalid"
+        pictureButtonPressed()
+        setupVision()
+        
+    }
+    func pictureButtonPressed(){
+        let scannerViewController = VNDocumentCameraViewController()
+        scannerViewController.delegate = self
+        present(scannerViewController, animated: true)
+    }
+     func setupVision() {
+        
+        textRecognitionRequest = VNRecognizeTextRequest { (request, error) in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            print("setupVision")
+            var detectedText = ""
+            for observation in observations {
+                guard let topCandidate = observation.topCandidates(1).first else { return }
+                print("text \(topCandidate.string) has confidence \(topCandidate.confidence)")
+                
+                detectedText += topCandidate.string
+                detectedText += "\n"
+            }
+            
+            DispatchQueue.main.async {
+                
+                 self.textView.text = detectedText
+                 self.textView.flashScrollIndicators()
+                 
+                 
+                //need to send the text & image over to ProcessImage view controller
+                
+                
+            }
+        }
+        
+        textRecognitionRequest.recognitionLevel = .accurate
+    }
+
+    private func processImage(_ image: UIImage) {
+        imageView.image = image
+        recognizeTextInImage(image)
     }
     
+    private func recognizeTextInImage(_ image: UIImage) {
+
+        print("ok")
+        
+        guard let cgImage = image.cgImage else { return }
+        
+        textView.text = ""
+        textRecognitionWorkQueue.async {
+            let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            do {
+                try requestHandler.perform([self.textRecognitionRequest])
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        guard scan.pageCount >= 1 else {
+            controller.dismiss(animated: true)
+            return
+        }
+        
+        let originalImage = scan.imageOfPage(at: 0)
+        let newImage = compressedImage(originalImage)
+        controller.dismiss(animated: true)
+        let image = scan.imageOfPage(at: 0)
+           let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+           do {
+               try handler.perform([textRecognitionRequest])
+           } catch {
+               print(error)
+           }
+        
+        processImage(image)
+    }
+
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+        print(error)
+        controller.dismiss(animated: true)
+    }
+
+    func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+        controller.dismiss(animated: true)
+    }
+
+    func compressedImage(_ originalImage: UIImage) -> UIImage {
+        guard let imageData = originalImage.jpegData(compressionQuality: 1),
+            let reloadedImage = UIImage(data: imageData) else {
+                return originalImage
+        }
+        return reloadedImage
+    }
 }
--- Scanner/Controller/ViewController.swift
+++ Scanner/Controller/ViewController.swift
@@ -11,28 +11,15 @@ import VisionKit
 import Vision
 import RealmSwift
 class ViewController: UITableViewController, VNDocumentCameraViewControllerDelegate {
-var textRecognitionRequest = VNRecognizeTextRequest(completionHandler: nil)
-private let textRecognitionWorkQueue = DispatchQueue(label: "MyVisionScannerQueue", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
-    var textRecognitionRequest = VNRecognizeTextRequest()
+
+let processedImage = ProcessedImageViewController()
 let realm = try! Realm()
 var text: Results<Data>?
     private var recognizedText: String?
 override func viewDidLoad() {
     super.viewDidLoad()
-    setupVision()
-    textRecognitionRequest = VNRecognizeTextRequest(completionHandler: { (request, error) in
-        if let results = request.results, !results.isEmpty {
-            if let requestResults = request.results as? [VNRecognizedTextObservation] {
-                self.recognizedText = ""
-                for observation in requestResults {
-                    guard let candidiate = observation.topCandidates(1).first else { return }
-                      self.recognizedText! += candidiate.string
-                    self.recognizedText! += "\n"
-                }
-                
-            }
-        }
-    })
+    
+    
 }
 
 func loadItems(){
@@ -57,123 +44,16 @@ func loadItems(){
     
     @IBAction func pictureButtonPressed(_ sender: UIBarButtonItem) {
     
-    let scannerViewController = VNDocumentCameraViewController()
-    scannerViewController.delegate = self
-    present(scannerViewController, animated: true)
-    DispatchQueue.main.async{
-        //create a new cell in the background
-    }
-}
-
-private func setupVision() {
-    textRecognitionRequest = VNRecognizeTextRequest { (request, error) in
-        guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
-        
-        var detectedText = ""
-        for observation in observations {
-            guard let topCandidate = observation.topCandidates(1).first else { return }
-            print("text \(topCandidate.string) has confidence \(topCandidate.confidence)")
-            
-            detectedText += topCandidate.string
-            detectedText += "\n"
-        }
-        
-        DispatchQueue.main.async {
-            /*
-             self.textView.text = detectedText
-             self.textView.flashScrollIndicators()
-             
-             */
-            //need to send the text & image over to ProcessImage view controller
-            
-            
-        }
-    }
+        processedImage.setupVision()
+    performSegue(withIdentifier: "toProcessedImage", sender: self)
     
-    textRecognitionRequest.recognitionLevel = .accurate
 }
 
-private func processImage(_ image: UIImage) {
-    //imageView.image = image
-    recognizeTextInImage(image)
-}
+
+
  
 
-private func recognizeTextInImage(_ image: UIImage) {
-    guard let cgImage = image.cgImage else { return }
-    print("ok")
-   textRecognitionRequest = VNRecognizeTextRequest(completionHandler: { (request, error) in
-       if let results = request.results, !results.isEmpty {
-           if let requestResults = request.results as? [VNRecognizedTextObservation] {
-               self.recognizedText = ""
-               for observation in requestResults {
-                   guard let candidiate = observation.topCandidates(1).first else { return }
-                     self.recognizedText! += candidiate.string
-                   self.recognizedText! += "\n"
-               }
-            do{ //need to save the recognized text to realm
-                let newData = Data()
-                newData.text = self.recognizedText!
-                self.saveItems(text: newData)
-                print("text saved")
-            }
-            
-                catch{
-                    print("Error saving text: \(error)")
-                }
-           }
-        self.tableView.reloadData()
-       }
-   })
-    
-    textRecognitionWorkQueue.async {
-        let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-        do {
-            try requestHandler.perform([self.textRecognitionRequest])
-           
-        } catch {
-            print(error)
-        }
-    }
-}
 
-
-func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
-    /*guard scan.pageCount >= 1 else {
-        controller.dismiss(animated: true)
-        return
-    }
-    
-    let originalImage = scan.imageOfPage(at: 0)
-    let newImage = compressedImage(originalImage)
-    controller.dismiss(animated: true)*/
-    let image = scan.imageOfPage(at: 0)
-       let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
-       do {
-           try handler.perform([textRecognitionRequest])
-       } catch {
-           print(error)
-       }
-    
-    processImage(image)
-}
-
-func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
-    print(error)
-    controller.dismiss(animated: true)
-}
-
-func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
-    controller.dismiss(animated: true)
-}
-
-func compressedImage(_ originalImage: UIImage) -> UIImage {
-    guard let imageData = originalImage.jpegData(compressionQuality: 1),
-        let reloadedImage = UIImage(data: imageData) else {
-            return originalImage
-    }
-    return reloadedImage
-}
 
 
 
